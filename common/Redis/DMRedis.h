092c55e25a9858054ba0931c92c102f78bf6fa9b
bing <563853086@qq.com>
2016-08-24 01:15:22 -0700

    add redis hash operation

--- common/Redis/DMRedis.h
+++ common/Redis/DMRedis.h
@@ -4,6 +4,7 @@
 #define REDIS_CFG_FILE "..//..//common//Redis//config.json"
 //各个进程单独实例化，多个实例同时连接redis
 //Redis所有API均使用同步操作
+//表数据采用hash结构存储   key为表名  domain为主键  value为字典形式行值{"":"","":""}
 typedef struct redis_config
 {
     std::string ip;
@@ -23,6 +24,10 @@ public:
     bool write_redis_string(std::string keys, std::string value);
 
     std::string read_redis_string(std::string keys);
+
+    bool write_redis_hash(std::string keys, std::string domain, std::string value);
+
+    std::string read_redis_hash(std::string keys, std::string domain);
 
     bool ping_redis();
     
